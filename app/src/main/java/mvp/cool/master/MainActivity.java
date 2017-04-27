a1c3556ae380ac9711d8f8642d59fec03d71db66
TanHao <tan_hao_shii@163.com>
2017-04-27 13:45:50 +0800

    handler

--- app/src/main/java/mvp/cool/master/MainActivity.java
+++ app/src/main/java/mvp/cool/master/MainActivity.java
@@ -1,7 +1,11 @@
 package mvp.cool.master;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.lang.ref.WeakReference;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,4 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    //避免handler造成的内存泄露
+    private static class MyHandler extends Handler {
+
+        private final WeakReference<MainActivity> mWeakReference;
+
+        private MyHandler(MainActivity mainActivty) {
+            mWeakReference = new WeakReference<MainActivity>(mainActivty);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            MainActivity mainActivty = mWeakReference.get();
+            if(mainActivty != null){
+                //
+            }
+        }
+    }
+
+    private final MyHandler mMyHandler = new MyHandler(this);
+
+    public static Runnable sRunnable = new Runnable() {
+        @Override
+        public void run() {
+            //
+        }
+    };
 }
